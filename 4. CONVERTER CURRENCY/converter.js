f7ba21bd43ffbb0d0b43963c471edba229572618
alisa_tumanskaia <alisa_tumanskaia@practice.relex.ru>
2024-05-30 00:45:10 +0300

    add 4 task

--- 4. CONVERTER CURRENCY/converter.js
+++ 4. CONVERTER CURRENCY/converter.js
@@ -30,7 +30,6 @@ const paramsJPY = {
     to: 'RUB'
 }
 
-
 const getCurrency = (params) => {
     return axios.get(_url, {params: params, headers: headers});
 }
@@ -41,7 +40,13 @@ const promise3 = getCurrency(paramsSGD);
 const promise4 = getCurrency(paramsMYR);
 const promise5 = getCurrency(paramsCAD);
 const promise6 = getCurrency(paramsJPY);
-Promise.all([promise1, promise2, promise3, promise4, promise5, promise6]).then(function (values) {
-    console.log(values);
-});
 
+function fetchData() {
+    Promise.all([promise1, promise2, promise3, promise4, promise5, promise6]).then(function (values) {
+        console.log(values);
+    });
+}
+
+fetchData()
+const minutes15 = 15000000
+setInterval(fetchData, minutes15)
