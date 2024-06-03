2ec5f2becbe5a4d236e106830b0c82e6fd1adaaa
alisa_tumanskaia <alisa_tumanskaia@practice.relex.ru>
2024-06-04 00:00:33 +0300

    second task

--- 2. FORM/form.js
+++ 2. FORM/form.js
@@ -7,8 +7,12 @@ const passwordRegex = '^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{4,8}$'
 inputPassword.addEventListener('keyup', (e) => checkPassword(e));
 
 let isValid = false;
+let value = ''
 
+const textErrorPassword = 'пароль должен содержать цифры, символы и буквы'
+const lengthErrorPassword = 'пароль должен быть не менее 4 символов и не более 8'
 const checkPassword = (e) => {
+    value = e.target.value
     e.target.value.match(passwordRegex) ? isValid = true : isValid = false
 }
 
@@ -19,9 +23,18 @@ const submitPassword = (e) => {
         passwordError.style.visibility = "hidden"
         inputPassword.classList.add('success')
     } else {
+        validateMessage()
         inputPassword.classList.remove('success')
         inputPassword.classList.add('error')
         passwordError.style.visibility = "visible"
+    }
+}
+
+const validateMessage = () => {
+    if (value.length < 4 || value.length > 8) {
+        passwordError.innerHTML = lengthErrorPassword
+    } else {
+        passwordError.innerHTML = textErrorPassword
     }
 }
 
