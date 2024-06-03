4e77cf0e501ef85127e2124d20660cb9061f3604
alisa_tumanskaia <alisa_tumanskaia@practice.relex.ru>
2024-06-03 15:54:25 +0300

    second task

--- 2. FORM/form.js
+++ 2. FORM/form.js
@@ -1,14 +1,28 @@
+const inputPassword = document.querySelector('.password')
+const form = document.querySelector('.contact__form');
+const btnSubmit = document.querySelector('.btn__submit');
+const passwordError = document.querySelector('.password__error');
+
 const passwordRegex = '^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{4,8}$'
-
-
-const inputText = document.querySelector('.contact__input')
-
-inputText.addEventListener('keyup', (e) => checkPassword(e));
+inputPassword.addEventListener('keyup', (e) => checkPassword(e));
 
 const checkPassword = (e) => {
-    e.target.value.match(passwordRegex)?
+    let isValid;
+    e.target.value.match(passwordRegex) ? isValid = true :  isValid = false
+    submitPassword(e, isValid)
+}
 
+const submitPassword = (e, isValid) => {
 
-        inputText.classList.add('success'): console.log("uuuuuuuuuu")
+    console.log(isValid, "isValid")
+    e.preventDefault();
+     isValid ?
+        inputPassword.classList.add('success')
+
+        :
+        inputPassword.classList.add('error')
+    passwordError.style.visibility="visible"
 
 }
+
+btnSubmit.addEventListener('click', submitPassword)
